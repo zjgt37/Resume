3b92cff1afd38b911958da3061591466e2ec24c8
zjgt37 <zjgt37@mst.edu>
2022-03-08 13:32:40 -0600

    Reverting changes for scroll speed

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        });
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
@@ -27,7 +27,7 @@
     $('#to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        });
+        }, 500);
     });
 
     // Scroll to first element
@@ -35,7 +35,7 @@
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        });
+        }, 500);
     });
 
     // Create timeline
